c24da985950c7209ae514227e4d16a539a8a1d4a
v.nikolayev <vn@insidedynamic.de>
2024-05-02 23:03:14 +0200

    done

--- HA020524/aufgabe.js
+++ HA020524/aufgabe.js
@@ -3,3 +3,30 @@ function convertToFarhenheit() {
     var fahrenheit = celsius * 9/5 + 32;
     console.log(celsius + " degrees Celsius is equal to " + fahrenheit + " degrees Fahrenheit.");
 }
+
+
+function aufgabe2(){
+
+
+    var autoListe = document.getElementById('autoMarken');
+
+    var automarken = ['Volkswagen', 'BMW', 'Mercedes-Benz', 'Audi', 'Ford'];
+    for (var i = 0; i < automarken.length; i++) {
+        var listenElement = document.createElement('li');
+        listenElement.textContent = automarken[i];
+        autoListe.appendChild(listenElement);
+    }
+    
+
+    var listenElemente = document.querySelectorAll('#autoMarken li');
+    for (var i = 0; i < listenElemente.length; i++) {
+        console.log(listenElemente[i].textContent);
+    }
+    
+
+
+
+}
+
+
+aufgabe2();
